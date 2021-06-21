47c8e62ecffbc9c6e992b4dcab9d61de731f2ad1
Sean Rycek <srycek@gmail.com>
2021-06-21 09:33:43 -0400

    Removed unnecessary React code from template. Modified existing React code to use functional components.

--- IPHeatmap/ClientApp/src/App.js
+++ IPHeatmap/ClientApp/src/App.js
@@ -1,22 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Route } from 'react-router';
-import { Layout } from './components/Layout/Layout';
-import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
+import Layout from './components/Layout/Layout';
+import Home from './components/Home/Home';
 
 import './custom.css'
 
-export default class App extends Component {
-  static displayName = App.name;
+const App = () => {
+    App.displayName = "App";
 
-  render () {
     return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
-      </Layout>
+        <Layout>
+            <Route exact path='/' component={Home} />
+        </Layout>
     );
-  }
-}
+};
+
+export default App;
---
+++ IPHeatmap/ClientApp/src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    Home.displayName = "Home";
+
+    return (
+        <div>
+        </div>
+    );
+
+};
+
+export default Home;
